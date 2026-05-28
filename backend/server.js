c1db61823b63db65b781175488a29a0331962f3b
sunilravulapati <sunilravulapati028@gmail.com>
2026-05-28 10:52:59 +0530

    added few dependencies for the frontend and allowed for cross origin requests

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const app = exp();
 //body parser middleware
 app.use(exp.json())
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true // Allow cookies to be sent in cross-origin requests
+}));
 app.use(cookieParser());
 
 //routes
